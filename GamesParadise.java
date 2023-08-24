aeca1e5c36acddc135142e8c889cbda1e69ed95a
TetianaHolovchenko <134524399+TetianaHolovchenko@users.noreply.github.com>
2023-08-24 14:46:03 -0500

    Add files via upload

---
+++ GamesParadise.java
@@ -0,0 +1,25 @@
+package gamesparadise;
+
+public class GamesParadise {
+	
+	String gamingSystem; 
+
+	boolean wireless;
+
+	Float price;
+
+	//create construct
+
+	public GamesParadise() {
+	}
+		// create a new dog object
+	
+	public GamesParadise(String gamingSystem, boolean wireless, Float price ) {
+
+		this.setBreed(breed);
+
+		this.setprice(price);
+
+		this.wireless(color);
+
+}
